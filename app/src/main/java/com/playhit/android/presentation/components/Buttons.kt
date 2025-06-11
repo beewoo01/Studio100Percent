56834235af6701bd49e47c5eb21ce72753bd0856
hyunwoo <beewoo01@naver.com>
2025-06-11 10:53:25 +0900

    Refactor: Modularzation Button

---
+++ app/src/main/java/com/playhit/android/presentation/components/Buttons.kt
@@ -0,0 +1,35 @@
+package com.playhit.android.presentation.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun DefaultBlackButton(title: String = "", onClick: () -> Unit = {}) {
+    return Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(50),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black
+        )
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.labelLarge,
+            color = Color.White
+        )
+    }
+}
--- app/src/main/java/com/playhit/android/presentation/components/RoundedTextField.kt
+++ app/src/main/java/com/playhit/android/presentation/components/RoundedTextField.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -67,3 +68,4 @@ fun RoundedTextFieldPreview() {
     val text = remember { mutableStateOf("") };
     RoundedTextField(value = text.value, onValueChange = {}, hint = "hint");
 }
+
