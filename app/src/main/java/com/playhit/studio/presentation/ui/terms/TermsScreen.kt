bc2a2aa38e6076bf132cc136655992f67551766b
hyunwoo <beewoo01@naver.com>
2025-06-12 11:40:30 +0900

    Feature: finish terms publisher

--- app/src/main/java/com/playhit/studio/presentation/ui/terms/TermsScreen.kt
+++ app/src/main/java/com/playhit/studio/presentation/ui/terms/TermsScreen.kt
@@ -1,4 +1,4 @@
-package com.playhit.android.presentation.ui.terms
+package com.playhit.studio.presentation.ui.terms
 
 import android.widget.Toast
 import androidx.compose.animation.animateColorAsState
@@ -33,11 +33,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.playhit.android.R
-import com.playhit.android.presentation.components.DefaultBlackButton
-import com.playhit.android.presentation.router.LocalNavScreenController
-import com.playhit.android.presentation.router.NavRoutes
-import com.playhit.android.presentation.theme.Studio100PercentTheme
+import com.playhit.studio.R
+import com.playhit.studio.presentation.components.DefaultBlackButton
+import com.playhit.studio.presentation.router.LocalNavScreenController
+import com.playhit.studio.presentation.router.NavRoutes
+import com.playhit.studio.presentation.theme.Studio100PercentTheme
 
 @Composable
 fun TermsScreen() {
