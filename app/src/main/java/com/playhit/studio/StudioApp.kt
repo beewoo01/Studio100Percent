6885feaa7888adddf02d2f3cf2522990c5ed547d
hyunwoo <beewoo01@naver.com>
2025-06-12 11:47:47 +0900

    Feature: setting application class for hilt

--- app/src/main/java/com/playhit/studio/StudioApp.kt
+++ app/src/main/java/com/playhit/studio/StudioApp.kt
@@ -1,4 +1,9 @@
 package com.playhit.studio
 
-class StudioApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StudioApp : Application() {
+
 }
