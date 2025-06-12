5612bb0d17e91ab7ce3ca787baa1dd703c6723ca
hyunwoo <beewoo01@naver.com>
2025-06-12 11:24:57 +0900

    Feature:  Build Default Buttons

--- app/src/main/java/com/playhit/studio/presentation/components/Buttons.kt
+++ app/src/main/java/com/playhit/studio/presentation/components/Buttons.kt
@@ -1,4 +1,4 @@
-package com.playhit.android.presentation.components
+package com.playhit.studio.presentation.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
