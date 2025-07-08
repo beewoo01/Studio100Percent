a32a986ee3a833e391acae2dee68da83679172fa
hyunwoo <beewoo01@naver.com>
2025-07-08 11:41:15 +0900

    Feature: made logo title bar

--- app/src/main/java/com/playhit/studio/presentation/components/LogoTitleBar.kt
+++ app/src/main/java/com/playhit/studio/presentation/components/LogoTitleBar.kt
@@ -1,10 +1,38 @@
 package com.playhit.studio.presentation.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.playhit.studio.R
 
 @Preview
 @Composable
-fun LogoTitleBar() {
+fun LogoTitleBar(modifier: Modifier = Modifier, title: String = "") {
+    Row(
+        modifier = modifier.padding(horizontal = 20.dp)
+    ) {
+        Image(
+            painterResource(R.drawable.white_logo),
+            modifier = modifier.size(16.dp),
+            contentDescription = "logo",
+        )
 
+        Spacer(modifier = modifier.width(width = 6.dp))
+
+        Text(
+            title,
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+    }
 }
